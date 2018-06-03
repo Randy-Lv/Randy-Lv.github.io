fe68e69498623c4d956e4ab45dc223f678495ab0
Randy-Lv <Randy-Lv@hotmail.com>
2018-06-03 16:34:05 +0800

    test

---
+++ js/async.js
@@ -0,0 +1,9 @@
+/**
+ * Created by LX on 2017/9/1.
+ */
+$('#header').load('cn/header.html');
+$('#banner').load('cn/banner.html');
+$('#about_us').load('cn/about_us.html');
+$('#know_us').load('cn/know_us.html');
+$('#products').load('cn/products.html');
+$('#footer').load('cn/footer.html');
