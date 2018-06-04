0a8ad075b9b2eff875fd4aeac3f424421741b838
Randy-Lv <Randy-Lv@hotmail.com>
2018-06-04 22:05:36 +0800

    删除注释

--- js/async.js
+++ js/async.js
@@ -1,6 +1,3 @@
-/**
- * Created by LX on 2017/9/1.
- */
 $('#header').load('cn/header.html');
 $('#banner').load('cn/banner.html');
 $('#about_us').load('cn/about_us.html');
--- js/async_en.js
+++ js/async_en.js
@@ -1,6 +1,3 @@
-/**
- * Created by LX on 2017/9/1.
- */
 $('#header').load('en/header.html');
 $('#banner').load('en/banner.html');
 $('#about_us').load('en/about_us.html');
