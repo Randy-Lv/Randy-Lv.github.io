a6ef87de73a42aa6d561dd47aa6b5908f4bcf566
Randy-Lv <Randy-Lv@hotmail.com>
2018-07-26 23:13:06 +0800

    弹出页面宽度、高度修改

--- js/alert.js
+++ js/alert.js
@@ -150,6 +150,15 @@ var jqueryAlert = function(opts){
 			'margin-left' : -parseInt($container.css('width'))/2
 		});
 
+		if(browserRedirect())
+		{
+			$container.css({
+				'margin-left' : 0,
+				'left': 0,
+				'width': '100%'
+			});
+		}		
+
 		if(option.animateType == 'scale'){
 			$container.addClass('bounceIn');
 		}
@@ -162,6 +171,24 @@ var jqueryAlert = function(opts){
 
 	};
 
+
+function browserRedirect() {
+    var sUserAgent = navigator.userAgent.toLowerCase();
+    var bIsIpad = sUserAgent.match(/ipad/i) == "ipad";
+    var bIsIphoneOs = sUserAgent.match(/iphone os/i) == "iphone os";
+    var bIsMidp = sUserAgent.match(/midp/i) == "midp";
+    var bIsUc7 = sUserAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4";
+    var bIsUc = sUserAgent.match(/ucweb/i) == "ucweb";
+    var bIsAndroid = sUserAgent.match(/android/i) == "android";
+    var bIsCE = sUserAgent.match(/windows ce/i) == "windows ce";
+    var bIsWM = sUserAgent.match(/windows mobile/i) == "windows mobile";
+    if (bIsIpad || bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsCE || bIsWM) {
+       return true;
+    } else {
+       return false;
+
+    }
+}
 	// 判断是否满足自动关闭的条件
 	function isSelfClose(){
 		if(dialog.buttonsLength <= 0 && option.style != 'pc'){
